e84e96ed9fd6cbacdbf8b820fbde50b97c6786c0
Alex Rodriguez <alexbrodriguez@gmail.com>
2015-07-27 17:25:35 +0000

    finished fwitter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,33 +9,67 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "flatiron2"
   end
-
+  
+  helpers do
+    
+    def signed_in?
+      session[:user_id]
+    end
+   
+    def current_user
+      current_user = User.find(session[:user_id])
+    end
+    
+    def error 
+      session[:error]
+    end
+    
+  end
   get '/' do
+    @signin_page = false
     @tweets = Tweet.all
     @users = User.all
     erb :index
   end
 
   get '/tweet' do
-    @users = User.all
+    @signin_page = false
+    @user = User.find_by(:id => session[:user_id])
     erb :tweet
   end
 
   post '/tweet' do
-    tweet = Tweet.new(:user_id => params[:user_id], :status => params[:status])
-    tweet.save
+    Tweet.create(:user_id => params[:user_id], :status => params[:status])
     redirect '/'
   end
 
   get '/users' do
-    @users = User.all
+    @signin_page = true
     erb :users
   end
 
   post '/sign-up' do
     @user = User.new(:name => params[:name], :email => params[:email])
     @user.save
+    redirect '/'
+  end
+
+  post '/sign-in' do
+    @user = User.find_by(:name => params[:name], :email => params[:email])
+    if @user
+      session[:user_id] = @user.id
+    end
+    session[:user_id]
+    redirect '/tweet'
+  end
+  
+  get '/sign-out' do
+    @signin_page = true
+    session[:user_id] = nil
+    session[:error] = nil
     redirect '/'
   end
   
